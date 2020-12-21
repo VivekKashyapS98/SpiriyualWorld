6adb24a53fece91a2f496717ce64916cb88538c9
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-12-21 14:25:16 +0530

    mobile optimisations

--- src/components/card.js
+++ src/components/card.js
@@ -10,7 +10,7 @@ export default function Card({ book }) {
     return <div className="card" onClick={() => handleCLick()}>
         <img className="card-image" src={book.image} alt={book.name}/>
         <h3>{book.name}</h3>
-        <p style={{color: '#999999'}}>{book.author}</p>
+        <p style={{color: '#999999', fontSize: 'x-small'}}>{book.author}</p>
         <h3>${book.price}</h3>
     </div>
 }
